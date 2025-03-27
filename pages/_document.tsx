6d0fd77fec12a940ee84dfa4ade0a94db00097d3
Leaping-AI-Services <leapingaiservices@gmail.com>
2025-03-27 19:03:42 +0300

    edits pwa

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -20,19 +20,17 @@ export default function Document() {
 				<script
 					dangerouslySetInnerHTML={{
 						__html: `
-              if ('serviceWorker' in navigator) {
-                window.addEventListener('load', function() {
-                  navigator.serviceWorker.register('/sw.js').then(
-                    function(registration) {
-                      console.log('ServiceWorker registration successful');
-                    },
-                    function(err) {
-                      console.log('ServiceWorker registration failed: ', err);
-                    }
-                  );
-                });
-              }
-            `,
+							if ('serviceWorker' in navigator) {
+								window.addEventListener('load', async function() {
+									try {
+										const registration = await navigator.serviceWorker.register('/sw.js');
+										console.log('ServiceWorker registration successful with scope:', registration.scope);
+									} catch (error) {
+										console.error('ServiceWorker registration failed:', error);
+									}
+								});
+							}
+						`,
 					}}
 				/>
 			</body>

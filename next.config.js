325b7252f869e4469f434752a383f56a787aa4d7
Leaping-AI-Services <leapingaiservices@gmail.com>
2025-04-05 03:51:40 +0300

    css form cashe disabled

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: true,
+	async headers() {
+		return [
+			{
+				source: '/_next/static/css/(.*)',
+				headers: [
+					{
+						key: 'Cache-Control',
+						value: 'no-store',
+					},
+				],
+			},
+		]
+	},
 }
 
 export default nextConfig
